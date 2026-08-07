bb8b675ae78ccae02c1a1786c2f4922e1a4fb699
rasansil <123409804+rasansil@users.noreply.github.com>
2026-08-07 11:04:18 -0300

    Refactor bronze_to_silver.py to remove unused code

--- scripts/02_bronze_to_silver.py
+++ scripts/02_bronze_to_silver.py
@@ -1,23 +1,12 @@
 from pathlib import Path
 import pandas as pd
-import sys
 
-# Adiciona a raiz do projeto ao PYTHONPATH
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(PROJECT_ROOT))
-
-from utils.database import (
-    get_connection,
-    publish_dataframe,
-    close_connection
-)
+from utils.database import publish_dataframe
 
 BRONZE_PATH = Path("bronze")
 SILVER_PATH = Path("silver")
 
 SILVER_PATH.mkdir(exist_ok=True)
-
-con = get_connection()
 
 for arquivo in BRONZE_PATH.glob("*.parquet"):
 
@@ -30,12 +19,9 @@ for arquivo in BRONZE_PATH.glob("*.parquet"):
     df.to_parquet(destino, index=False)
 
     publish_dataframe(
-        con,
         df,
         "silver",
         arquivo.stem
     )
 
-close_connection(con)
-
-print("Camada Silver criada com sucesso.")
+print("Silver criada com sucesso.")
